75bb60f717cad1c378e877d09c1711b3d1abdee7
ofarukcelik <ofarukcelik@outlook.com>
2021-02-17 00:07:04 +0300

    new properties added

--- GameProject/Entities/Gamer.cs
+++ GameProject/Entities/Gamer.cs
@@ -6,5 +6,10 @@ namespace GameProject.Entities
 {
     public class Gamer
     {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string NationalityID { get; set; }
     }
 }
