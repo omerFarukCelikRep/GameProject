e7f64a8342b862ff04734d0b68993e96fad5f137
ofarukcelik <ofarukcelik@outlook.com>
2021-02-17 01:36:54 +0300

    Add, Update, Delete methods simulated

--- GameProject/Program.cs
+++ GameProject/Program.cs
@@ -1,4 +1,6 @@
-﻿using GameProject.Concrete;
+﻿using GameProject.Abstract;
+using GameProject.Adapters;
+using GameProject.Concrete;
 using GameProject.Entities;
 using System;
 
@@ -8,8 +10,30 @@ namespace GameProject
     {
         static void Main(string[] args)
         {
-            CustomerManager customerManager = new CustomerManager();
-            customerManager.Update(new Customer { FirstName = "Ömer Faruk" });
+            Customer customer1 = new Customer
+            {
+                ID = 1,
+                FirstName = "Ömer Faruk",
+                LastName = "Çelik",
+                DateOfBirth = new DateTime(1995, 1, 1),
+                NationalityID = "12345678901"
+            };
+
+            Customer customer2 = new Customer
+            {
+                ID = 2,
+                FirstName = "Ali",
+                LastName = "Veli",
+                DateOfBirth = new DateTime(1986, 1, 1),
+                NationalityID = "12345678901"
+            };
+
+            BaseCustomerManager customerManager = new CustomerManager(new EDevletServiceAdapter());
+            customerManager.Add(customer1);
+            customerManager.Add(customer2);
+
+            customerManager.Update(customer2);
+            customerManager.Delete(customer1);
         }
     }
 }
