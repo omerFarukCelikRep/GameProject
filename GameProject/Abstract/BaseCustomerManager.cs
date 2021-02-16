15437dee81bbedf1972ca22ed53d69248e38432d
ofarukcelik <ofarukcelik@outlook.com>
2021-02-17 01:31:18 +0300

    Customer Manager methods overrided

--- GameProject/Abstract/BaseCustomerManager.cs
+++ GameProject/Abstract/BaseCustomerManager.cs
@@ -7,17 +7,17 @@ namespace GameProject.Abstract
 {
     public abstract class BaseCustomerManager : ICustomerService
     {
-        public void Add(Customer customer)
+        public virtual void Add(Customer customer)
         {
             Console.WriteLine(customer.FirstName + " Added");
         }
 
-        public void Delete(Customer customer)
+        public virtual void Delete(Customer customer)
         {
             Console.WriteLine(customer.FirstName + " Deleted");
         }
 
-        public void Update(Customer customer)
+        public virtual void Update(Customer customer)
         {
             Console.WriteLine(customer.FirstName + " Updated");
         }
--- GameProject/Concrete/CustomerManager.cs
+++ GameProject/Concrete/CustomerManager.cs
@@ -1,4 +1,5 @@
 ﻿using GameProject.Abstract;
+using GameProject.Entities;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,6 +8,33 @@ namespace GameProject.Concrete
 {
     public class CustomerManager : BaseCustomerManager
     {
+        private ICustomerCheckService _customerCheckService;
 
+        public CustomerManager(ICustomerCheckService customerCheckService)
+        {
+            _customerCheckService = customerCheckService;
+        }
+
+        public override void Add(Customer customer)
+        {
+            if (_customerCheckService.CheckIfReal(customer))
+            {
+                base.Add(customer); 
+            }
+            else
+            {
+                throw new Exception("Not a Valid Exception");
+            }
+        }
+
+        public override void Update(Customer customer)
+        {
+            base.Update(customer);
+        }
+
+        public override void Delete(Customer customer)
+        {
+            base.Delete(customer);
+        }
     }
 }
