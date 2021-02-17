cd79a7b99b7dcead16e19e87a37f7211ab2d29bf
ofarukcelik <ofarukcelik@outlook.com>
2021-02-17 16:08:38 +0300

    GameManager class implemented from IGameService

---
+++ GameProject/Concrete/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using GameProject.Abstract;
+using GameProject.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject.Adapters
+{
+    public class GameManager : IGameService
+    {
+        public void Sell(Customer customer, Game game)
+        {
+            Console.WriteLine(game.Name + " Sold to " + customer.FirstName + " " + customer.LastName);
+        }
+    }
+}
