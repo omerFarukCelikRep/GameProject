54cbc47a6b391b66c82c279efefc30a6ed04f57d
ofarukcelik <ofarukcelik@outlook.com>
2021-02-17 16:06:08 +0300

    IGameService interface and Sell method created

---
+++ GameProject/Abstract/IGameService.cs
@@ -0,0 +1,12 @@
+﻿using GameProject.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject.Abstract
+{
+    public interface IGameService
+    {
+        void Sell(Customer customer, Game game);
+    }
+}
