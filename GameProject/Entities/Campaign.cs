097bd164ddc5447449aad64308a0a2b3fd1bf6d6
ofarukcelik <ofarukcelik@outlook.com>
2021-02-17 17:09:40 +0300

    Campaign class and properties created

---
+++ GameProject/Entities/Campaign.cs
@@ -0,0 +1,15 @@
+﻿using GameProject.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject.Entities
+{
+    public class Campaign : IEntity
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
