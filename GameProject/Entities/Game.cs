59f7dbaadaa470b51d82360cf8119150e61030d9
ofarukcelik <ofarukcelik@outlook.com>
2021-02-17 15:58:54 +0300

    Game class created

---
+++ GameProject/Entities/Game.cs
@@ -0,0 +1,12 @@
+﻿using GameProject.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject.Entities
+{
+    public class Game : IEntity
+    {
+
+    }
+}
